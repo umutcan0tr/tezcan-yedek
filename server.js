d33fe2bc3d5f6e7f5604e134354676a437573ab2
Glitch (tezcan-yede) <none>
2020-05-29 20:04:36 +0000

    🎅🦃 Checkpoint ./komutlar/kayıt-ol.js:16323033/1487 ./komutlar/kayıt.js:16323033/1487 ./server.js:16323033/12039

---
+++ server.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const ayarlar = require("./ayarlar.json");
+const chalk = require('chalk');
+client.queue = new Map();
+const fs = require("fs");
+const db = require("quick.db");
+const moment = require("moment");
+require("./util/eventLoader")(client);
+
+client.on("message", async msg => {
+  db.fetch(`kufur_${msg.guild.id}`).then(i => {
+if (i == 'Açık') {
+        const kufur = ["amk","a.m.k","am","a.m","m.k","mk","orosbu çocugu","orospu çocugu","o.ç","oç","oc","o.c","orosbu","orospu","veledi","zina","sikerim","sıkerım","s.i.k.e.r.i.m","s.ı.k.e.r.ı.m","piç","pıc","p.i.ç","p.ı.c","orosbu evladı","orospu evladı","amına koyayım","babanı sikim","sik","s.ik","si.k","s.i.k", "amk", "amcık", "yarrak", "sik", "amına koyduğum", "kaltak", "yavşak", "orospu", "piç", "ananı sikim", "sikik", "göt", "pezevenk", "gavat", "meme"];
+        if (kufur.some(word => msg.content.includes(word))) {
+          try {
+             if (!msg.member.hasPermission("BAN_MEMBERS")) {
+                  
+     
+               
+               msg.delete(); 
+             
+
+                  return msg.reply('Küfür etme amına kodumun salağı.').then(msg => msg.delete(3000));
+             }
+          } catch(err) {
+            console.log(err);
+          }
+        } } else if (i == 'Kapalı') {
+
+}
+
+})
+});
