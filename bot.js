a1cbaf7b784158ee97cd6d30aca3f7d39a6a73ee
Glitch (tezcan-yedek) <none>
2020-05-31 21:54:04 +0000

    🍽🌦 Checkpoint ./bot.js:16323033/144291

--- bot.js
+++ bot.js
@@ -404,3 +404,183 @@ var sabit = client.emojis.get(`609086032276619295`)
 client.login(ayarlar.token);
 
 //EKSTRA KODLAR BU TARAFLARA EKLENECEK!
+
+client.queue = new Map();
+const db = require("quick.db");
+require("./util/eventLoader")(client);
+
+///////////
+const http = require("http");
+const path = require("path");
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping tamamdır.");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+///////////
+
+client.on('guildMemberAdd', async member => {
+  
+  let tag = await db.fetch(`tag_${member.guild.id}`);
+  let tagyazi;
+  if (tag == null) tagyazi = member.setNickname(`${member.user.username}`)
+  else tagyazi = member.setNickname(`${tag} | ${member.user.username}`)
+});
+
+////////////////////////
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === 'sa') {
+    msg.reply('Aleyküm selam,  hoş geldin ^^');
+  }
+});
+
+client.elevation = message => {
+  if(!message.guild) {
+	return; }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.sahip) permlvl = 4;
+  return permlvl;
+};
+
+
+///////////////////////////////////////////////////
+
+client.on("message", async message => {
+    let afk_kullanici = message.mentions.users.first() || message.author;
+    if(message.content.startsWith("!afk")) return; //! yazan yeri kendi botunuzun prefixi ile değiştirin.
+  if (message.author.bot === true) return;
+    if(message.content.includes(`<@${afk_kullanici.id}>`))
+        if(await db.fetch(`afks_${afk_kullanici.id}`)) {
+                message.channel.send(`**${client.users.get(afk_kullanici.id).tag}** adlı kullanıcı şuanda AFK! \n**Sebep:** \n${await db.fetch(`afks_${afk_kullanici.id}`)}`)
+        }
+        if(await db.fetch(`afks_${message.author.id}`)) {
+                message.reply("başarıyla AFK modundan çıktın!")
+            db.delete(`afks_${message.author.id}`)
+        }
+});
+
+///////////////////////////////////////////////////
+
+client.on("message",message => {
+  if(!message.author.bot) return;
+  db.fetch(`usohbet_${message.channel.id}`).then(usdurum => {
+    if(!usdurum || usdurum === 'pasif') return;
+    else {
+      message.delete(3500)
+    }
+})})
+
+///////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////
+
+///////////////////////////////////////////////////
+
+client.on('guildMemberAdd', member => {
+ let guvenlik= db.fetch(`bottemizle_${member.guild.id}`)
+    if (!guvenlik) return;
+    if(member.user.bot !==true){
+    } else {
+   member.kick(member) 
+  }  
+  });
+
+///////////////////////////////////////////////////
+
+client.on("message", async msg => {
+  db.fetch(`kufur_${msg.guild.id}`).then(i => {
+if (i == 'Açık') {
+        const kufur = ["discord.gg","https//",".com",".xyz",".net"];
+        if (kufur.some(word => msg.content.includes(word))) {
+          try {
+             if (!msg.member.hasPermission("BAN_MEMBERS")) {
+                  
+     
+               
+               msg.delete(); 
+             
+
+                  return msg.reply('Reklam yapmamalısın.').then(msg => msg.delete(3000));
+             }
+          } catch(err) {
+            console.log(err);
+          }
+        } } else if (i == 'Kapalı') {
+
+}
+
+})
+});
+
+///////////////////////////////////////////////////
+
+client.on("message", async msg => {
+
+
+  const i = await db.fetch(`ssaass_${msg.guild.id}`);
+    if (i == 'acik') {
+      if (msg.content.toLowerCase() == 'selam' || msg.content.toLowerCase() == 'sa' || 
+msg.content.toLowerCase() == 'Selam Naber') {
+          try {
+
+                  return msg.reply('Aleyküm Selam')
+          } catch(err) {
+            console.log(err);
+          }
+      }
+    }
+    else if (i == 'kapali') {
+    
+    }
+    if (!i) return;
+
+    });
+
+///////////////////////////////////////////////////
+
+client.on("message", async msg => {
+  db.fetch(`kufur_${msg.guild.id}`).then(i => {
+if (i == 'Açık') {
+        const kufur = ["amk","a.m.k","am","a.m","m.k","mk","orosbu çocugu","orospu çocugu","o.ç","oç","oc","o.c","orosbu","orospu","veledi","zina","sikerim","sıkerım","s.i.k.e.r.i.m","s.ı.k.e.r.ı.m","piç","pıc","p.i.ç","p.ı.c","orosbu evladı","orospu evladı","amına koyayım","babanı sikim","sik","s.ik","si.k","s.i.k"];
+        if (kufur.some(word => msg.content.includes(word))) {
+          try {
+             if (!msg.member.hasPermission("BAN_MEMBERS")) {
+                  
+     
+               
+               msg.delete(); 
+             
+
+                  return msg.reply('Küfür etme amına kodumun salağı.').then(msg => msg.delete(3000));
+             }
+          } catch(err) {
+            console.log(err);
+          }
+        } } else if (i == 'Kapalı') {
+
+}
+
+})
+});
+
+///////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////
+
+///////////////////////////////////////////////////
+
+///////////////////////////////////////////////////
+
+///////////////////////////////////////////////////
